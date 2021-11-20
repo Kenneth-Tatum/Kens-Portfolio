73505a95ba7974976d51706f67cdd6ba5fc3c32e
Smoke94 <87729707+Smoke94@users.noreply.github.com>
2021-11-20 00:04:54 -0500

    Slowed button Transition and updated into text

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ intro.onmouseover = changeIntro
 intro.onmouseout = changeBack
 function changeIntro() {
     intro.innerHTML = "Hello and welcome to my portfolio!"
+    intro.style.fontSize="50px"
+    intro.style.backgroundColor="#34E4C9"
 }
 function changeBack() {
     intro.innerHTML = "I'm a self-taught soon to be Software Engineer."
+    intro.style.fontSize="25px"
+    intro.style.backgroundColor=""
 }
