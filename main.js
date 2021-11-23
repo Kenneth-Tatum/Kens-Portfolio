a7bec77f371fc0f617ce639e8c9e76f627a9e2f8
Smoke94 <87729707+Smoke94@users.noreply.github.com>
2021-11-23 00:17:57 -0500

    Grabbing all list and button elements through js methods

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+// changing the intro head onmouseover and mouseout
 var intro = document.getElementById("intro")
 intro.onmouseover = changeIntro
 intro.onmouseout = changeBack
+
 function changeIntro() {
     intro.innerHTML = "Hello and welcome to my portfolio!"
     intro.style.fontSize="50px"
@@ -10,4 +12,25 @@ function changeBack() {
     intro.innerHTML = "I'm a self-taught soon to be Software Engineer."
     intro.style.fontSize="25px"
     intro.style.backgroundColor=""
+}
+// grabbing all button elements
+var htmlBut = document.getElementById("html")
+var cssBut = document.getElementById("css")
+var javaBut = document.getElementById("java")
+
+// grabbing all repective list elements
+var htmlItems = document.querySelectorAll("htmlItem")
+var cssItems = document.querySelectorAll("cssItem")
+var jsItems = document.querySelectorAll("jsItem")
+
+function changeHTML() {
+    htmlItems
+}
+
+function changeCSS() {
+    cssItems
+}
+
+function changeJs() {
+    jsItems
 }
